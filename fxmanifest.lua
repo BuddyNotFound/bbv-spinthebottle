bb3f6f7ae5313cbda9038428218a855c396538dc
PatalJunior <81658559+PatalJunior@users.noreply.github.com>
2024-06-13 05:19:39 +0100

    ox_lib locale

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,8 @@ game 'gta5'
 description 'bbv-spinthebottle'
 version '1.0.0'
 
+ox_lib 'locale'
+
 client_scripts {
     'wrapper/cl_wrapper.lua',
     'client/cl_main.lua',
@@ -14,8 +16,21 @@ server_scripts {
 }
 
 shared_scripts {
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/lib.lua',
     'config.lua',
 }
+
+files {
+    'locales/*.json'
+}
+
+
+dependencies {
+    'ox_lib',
+}
+
+
 
 
 lua54 'yes'
